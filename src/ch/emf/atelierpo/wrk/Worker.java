ef9863bdff1f7e85a8cb6e36f0fa5e1eaf45d807
blanchardy <blanchardy@WSTEMFA44-17.stu.net.fr.ch>
2024-04-17 09:05:01 +0200

    javadoc finie

--- src/ch/emf/atelierpo/wrk/Worker.java
+++ src/ch/emf/atelierpo/wrk/Worker.java
@@ -2,12 +2,23 @@ package ch.emf.atelierpo.wrk;
 
 /**
  *
- * @author Mario Ramalho(EMF - Fribourg)
+ * @author Yann Blanchard
  */
 public class Worker {
 
-   
+    /**
+     * Description : Vérifier si la plaque d'immatriculation est correct. Pour
+     * qu'une plaque soit correcte il faut que le canton soit parmis les
+     * suivants : AG, AI, AR, BE, BL, BS, FR, GE, GL, GR, JU, LU, NE, NW, OW,
+     * SG, SH, SO, SZ, TG, TI, UR, VD, VS, ZG, ZH donc que les deux lettres
+     * soient en majuscule et sans chiffres. Pour le numéro de plaque il faut
+     * qu'il n'y ai que deux caractères et sans lettre
+     * @param canton
+     * @param numeroDePlaque
+     * @return true si la plaque est correct ou false si incorrect
+     */
     public boolean controlePlaqueImmatriculation(String canton, String numeroDePlaque) {
+
         return false;
     }
 
